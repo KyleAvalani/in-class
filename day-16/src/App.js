088d240319c4cece3b626464f2d7a93384e20cb3
Kyle <kyleavalani@msn.com>
2018-11-20 11:14:25 -0800

    FInish in class

--- day-16/src/App.js
+++ day-16/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import { HashRouter as Router, Route, Link } from "react-router-dom";
+import {Homepage} from './Homepage';
+import {About} from './About';
+import {Projects} from './Projects';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 // Import your other components here
@@ -8,7 +11,16 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 class App extends Component {
     render() {
         return (<div>
-                  { /*Render your Router, Links, and Routes in here */ }
+                  <Router>
+                      <div className='container'>
+                      <Link to='/'>Home</Link>
+                      <Link to='/about'>About</Link>
+                      <Link to='/projects'>Projects</Link>
+                      <Route exact path='/' component={Homepage} />
+                      <Route path='/projects' component={Projects} />
+                      <Route path='/about' component={About} />
+                      </div>
+                  </Router>
                 </div>
             );
     }
