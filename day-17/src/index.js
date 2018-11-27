dd5a696adfa5b8fbbdcf33a47a7383f1fa1f5c35
Kyle <kyleavalani@msn.com>
2018-11-27 11:20:39 -0800

    Finish exercises in class stuff

--- day-17/src/index.js
+++ day-17/src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase/app';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+const config = {
+    apiKey: "AIzaSyBtWe_zW2C6JdM9zVEqZlX6w5Dw12xmTvo",
+    authDomain: "info340inclass.firebaseapp.com",
+    databaseURL: "https://info340inclass.firebaseio.com",
+    projectId: "info340inclass",
+    storageBucket: "info340inclass.appspot.com",
+    messagingSenderId: "61264892217"
+  };
+
+firebase.initializeApp(config);
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
